dc45b667fa30240740cb2b0865c795eb56655141
gityhb <qldpdlvl0206@naver.com>
2025-11-20 09:57:31 +0900

    배포 도메인 권한

--- sugang-api/src/main/java/com/sugang/coursepage/WebConfig.java
+++ sugang-api/src/main/java/com/sugang/coursepage/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                     "https://sugang-api-429428399883.asia-northeast3.run.app",
-                    "http://localhost:3000"
+                    "http://localhost:3000",
+                    "https://course-registration-virid-three.vercel.app/" //배포한 vercel 주소
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
